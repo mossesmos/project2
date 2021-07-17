c84116c3be62097112c75211eb95888341049262
mossesmos <mossesmos@gmail.com>
2021-07-17 10:01:26 -0700

    edited greeter

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -30,16 +30,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
+    String someone = "World , Thankyou";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-@Test
-  public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
+  
